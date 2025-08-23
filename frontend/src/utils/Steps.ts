99c561c5430544221f85efaf88538ecf6c736082
08abhinav <abhinavnegi101@gmail.com>
2025-08-23 18:07:13 +0530

    steps utils

---
+++ frontend/src/utils/Steps.ts
@@ -0,0 +1,17 @@
+export const steps = [
+    {
+      id: 1,
+      title: "📂 Upload Your Files",
+      description: "Upload PDFs, DOCX, CSVs, or share a URL. RAGify will process and store them securely.",
+    },
+    {
+      id: 2,
+      title: "❓ Ask Questions",
+      description: "Type any query related to your uploaded files. No need to scroll through endless pages.",
+    },
+    {
+      id: 3,
+      title: "💡 Get Instant Insights",
+      description: "Receive clear, AI-powered answers and summaries based on your document content.",
+    },
+  ]
