8bacba7b33130b8df1d7eedef08b2a3ca3587643
08abhinav <abhinavnegi101@gmail.com>
2026-01-20 11:08:24 +0530

    update: changed langchain upload and update the accordingly

--- backend/controllers/web_controller.py
+++ backend/controllers/web_controller.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from state import save_vectors
 from langchain_community.document_loaders import WebBaseLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 # Load environment variables
 load_dotenv()
@@ -17,16 +17,17 @@ def process_web(url, doc_id):
         ).load()
 
         # Split into chunks
-        splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size=500, 
+            chunk_overlap=100
+        )
         chunks = splitter.split_documents(documents)
 
         # Attach doc_id metadata to every chunk
         for chunk in chunks:
             chunk.metadata["doc_id"] = doc_id
 
-        # Save into Qdrant
-        save_vectors(chunks, doc_id)
-        return True
+        return chunks
 
     except Exception as e:
         raise RuntimeError(f"Error processing web page: {str(e)}")
