24db83cb4d8dcf2c24a45b8788bfe3036a67c7b2
08abhinav <abhinavnegi101@gmail.com>
2026-01-24 14:49:31 +0530

    app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,17 @@
 import { Route, Routes } from "react-router-dom"
-import { Home } from "./pages/Home"
 import { Rag } from "./pages/Rag"
 import { Explore } from "./pages/Explore"
+import Index from "./pages/Index"
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Index/>}/>
         <Route path="/rag" element={<Rag/>}/>
         <Route path="/explore" element={<Explore/>}/>
       </Routes>
     </>
   )
 }
-
 export default App
